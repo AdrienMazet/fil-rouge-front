d82e2f9a18b9709b9fb469c0cb273e17d38d1a1d
AdrienMazet <Adrien.mazet.56@gmail.com>
2021-12-17 15:06:36 +0000

    limit pannign

--- src/components/LeafletMap.tsx
+++ src/components/LeafletMap.tsx
@@ -1,44 +1,48 @@
 import L, { CRS } from "leaflet";
-import icon from 'leaflet/dist/images/marker-icon.png';
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
-import 'leaflet/dist/leaflet.css';
-import { useEffect, useMemo } from "react";
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+import "leaflet/dist/leaflet.css";
+import { useEffect } from "react";
 import {
     ImageOverlay,
-    MapContainer, Marker,
-    Popup, useMap
+    MapContainer,
+    Marker,
+    Popup,
+    useMap
 } from "react-leaflet";
 
-
 let DefaultIcon = L.icon({
-    iconUrl: icon,
-    shadowUrl: iconShadow
+  iconUrl: icon,
+  shadowUrl: iconShadow,
 });
 
 L.Marker.prototype.options.icon = DefaultIcon;
 
-
-const CustomMap = () => {
-  const map = useMap();
-  const bounds = useMemo(
-    () =>
-      [
+const bounds = [
         [-26.5, -25],
         [1021.5, 1023],
-      ] as any,
-    []
-  );
+      ] as any
+
+const CustomMap = () => {
+  const map = useMap(); 
   useEffect(() => {
     map.fitBounds(bounds);
-  }, [map, bounds]);
+  }, [map]);
 
   return <ImageOverlay url="./office.jpg" bounds={bounds} />;
 };
 const LeafletMap = () => (
   <>
-    <MapContainer minZoom={0} maxZoom={2} crs={CRS.Simple} style={{ height: "100vh", backgroundColor: "white" }}>
+    <MapContainer
+      minZoom={0}
+      maxZoom={2}
+      crs={CRS.Simple}
+      style={{ height: "100vh", backgroundColor: "white" }}
+      maxBoundsViscosity={1.0}
+      maxBounds={bounds}
+    >
       <CustomMap />
-      <Marker position={[500,500]}>
+      <Marker position={[500, 500]}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
